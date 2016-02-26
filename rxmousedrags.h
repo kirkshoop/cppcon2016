d93b1fb4272d43f2f59e280d9483ad7ed7d63e57
Kirk Shoop <kirk.shoop@microsoft.com>
2016-02-26 12:02:26 +0300

    add default mouse drag automation

--- rxmousedrags.h
+++ rxmousedrags.h
@@ -20,7 +20,27 @@ void rxmousedrags()
                     sum();
             }).
             merge().
+            map( 
+                [](int c){
+                    return to_string(c) + " moves while mouse down";
+                }).
             subscribe( 
-                [](int count){cout << count << " moves while mouse down" << endl;},
+                println(cout),
                 [](exception_ptr ep){cout << what(ep) << endl;}));
+
+  EM_ASM(
+    function sendEvent(type, data) {
+      var event = document.createEvent('Event');
+      event.initEvent(type, true, true);
+      for(var d in data) event[d] = data[d];
+      window.dispatchEvent(event);
+    }
+    sendEvent('mousemove', { screenX: 1, screenY: 1, clientX: 1, clientY: 1, button: 0, buttons: 0, 'movementX': 1, 'movementY': 1 });
+    sendEvent('mousemove', { screenX: 1, screenY: 1, clientX: 1, clientY: 1, button: 0, buttons: 0, 'movementX': 1, 'movementY': 1 });
+    sendEvent('mousedown', { screenX: 1, screenY: 1, clientX: 1, clientY: 1, button: 0, buttons: 1 });
+    sendEvent('mousemove', { screenX: 1, screenY: 1, clientX: 1, clientY: 1, button: 0, buttons: 0, 'movementX': 1, 'movementY': 1 });
+    sendEvent('mousemove', { screenX: 1, screenY: 1, clientX: 1, clientY: 1, button: 0, buttons: 0, 'movementX': 1, 'movementY': 1 });
+    sendEvent('mouseup', { screenX: 1, screenY: 1, clientX: 1, clientY: 1, button: 0, buttons: 0 });
+    sendEvent('mousemove', { screenX: 1, screenY: 1, clientX: 1, clientY: 1, button: 0, buttons: 0, 'movementX': 1, 'movementY': 1 });
+  );
 }
